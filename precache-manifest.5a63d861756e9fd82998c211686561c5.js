525592af03ce55a295bb7fa5984027405048717d
admin <adimin@mail.ru>
2023-05-27 14:50:17 +0800

    Updates

---
+++ precache-manifest.5a63d861756e9fd82998c211686561c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d79776ee99c0b9a99d223a677c3288",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "8bd69e2a4bbd713ce9e4",
+    "url": "/todo-react/static/css/main.d9a8cd82.chunk.css"
+  },
+  {
+    "revision": "3b9a09ff2c949a839228",
+    "url": "/todo-react/static/js/2.8239ff3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.8239ff3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bd69e2a4bbd713ce9e4",
+    "url": "/todo-react/static/js/main.1919b1aa.chunk.js"
+  },
+  {
+    "revision": "383ba90748c342dd3b71",
+    "url": "/todo-react/static/js/runtime-main.4e144f66.js"
+  }
+]);
